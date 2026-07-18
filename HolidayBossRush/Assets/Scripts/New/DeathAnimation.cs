8fa34751cf88cd6ed705931a8a0fd159db45fac4
Isaac <Isaac.lee10123@gmail.com>
2026-07-18 17:30:02 -0400

    did a lot

--- HolidayBossRush/Assets/Scripts/New/DeathAnimation.cs
+++ HolidayBossRush/Assets/Scripts/New/DeathAnimation.cs
@@ -6,22 +6,37 @@ using UnityEngine;
 public class DeathAnimation : MonoBehaviour
 {
     [SerializeField] private float addForce;
+    [SerializeField] private Sprite deathSprite;
 
     private Rigidbody2D _rigidbody2D;
     private Collider2D[] _colliders;
+    
+    [SerializeField] private Component[] components;
 
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    
     public void Death()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _rigidbody2D.isKinematic = false;
+        _rigidbody2D.velocity = Vector3.zero;
         _rigidbody2D.AddForce(Vector2.up * addForce, ForceMode2D.Impulse);
+
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.sprite = deathSprite;
         
         _colliders = GetComponents<Collider2D>();
-        foreach (Collider2D singleCollider in _colliders)
+        foreach (Component component in components)
         {
-            singleCollider.enabled = false;
+            if (component is Behaviour componentBehaviour)
+            {
+                Debug.Log(componentBehaviour);
+                componentBehaviour.enabled = false;
+            }
         }
         
-        Invoke("Destroy", 3);
+        Invoke("Destroy", 5f);
     }
     
     private void Destroy()
